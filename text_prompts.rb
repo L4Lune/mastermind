869c22b0e1fc9cd0f1380a0e965bab577e5ad785
Jack Roudebush <roudebushjack@gmail.com>
2023-05-07 15:51:23 -0400

    Add incorrect_guess method

--- text_prompts.rb
+++ text_prompts.rb
@@ -1,4 +1,8 @@
 module TextPrompts
+  def incorrect_guess
+    p 'That was not the code. Please try again.'
+  end
+
   def last_turn
     p 'This is the last chance to guess the code! Good luck!'
   end
