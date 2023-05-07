26297c113119c8f327eb1680ef99d217629ebf00
Jack Roudebush <roudebushjack@gmail.com>
2023-05-06 23:33:24 -0400

    Add basic loop structure for the game

--- game.rb
+++ game.rb
@@ -1,9 +1,19 @@
 class Game
-  inclide Icons
+  # include Icons
   attr_reader :human, :computer
+  attr_accessor :rounds
 
   def initialize
     @human = Player.new
     @computer = CompPlayer.new
+    @rounds = 12
+  end
+
+  def play
+    while self.rounds.positive?
+      human.guess=()
+      self.rounds -= 1
+      rounds
+    end
   end
 end
