5990ec18b497720773166ec3bb183a6948ece949
Jack Roudebush <roudebushjack@gmail.com>
2023-05-06 23:33:02 -0400

    Refactor comp_player class to create code and transform it to string array upon initialization

--- comp_player.rb
+++ comp_player.rb
@@ -2,10 +2,10 @@ class CompPlayer
   attr_reader :code
 
   def initialize
-    @code = code
+    @code = (4.times.map { rand(1..6) }).map(&:to_s)
   end
 
-  def create_code
-    p (4.times.map { rand(1..6) })
-  end
+  # def transform_code
+  #   code.map(&:to_s)
+  # end
 end
