1057d55d9e21e2728c47631890c7a137cef698e1
Jack Roudebush <roudebushjack@gmail.com>
2023-05-06 22:10:56 -0400

    Create game class and refactor CompPlayer class method

--- comp_player.rb
+++ comp_player.rb
@@ -1,12 +1,11 @@
 class CompPlayer
   attr_reader :code
-  
+
   def initialize
     @code = code
   end
 
   def create_code
-    code = (4.times.map { rand(1..6) })
-    p code
+    p (4.times.map { rand(1..6) })
   end
 end
