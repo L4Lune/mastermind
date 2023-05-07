5b982da15de3a70bd1d79bc94f9631eb20cee61f
Jack Roudebush <roudebushjack@gmail.com>
2023-05-06 23:37:12 -0400

    Add code_correct? method to compare code to guess

--- comp_player.rb
+++ comp_player.rb
@@ -5,6 +5,14 @@ class CompPlayer
     @code = (4.times.map { rand(1..6) }).map(&:to_s)
   end
 
+  def code_correct?(user_guess)
+    if user_guess == code
+      return true
+    elsif user_guess != code
+      return false
+    end
+  end
+
   # def transform_code
   #   code.map(&:to_s)
   # end
