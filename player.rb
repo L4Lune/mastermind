00d68ef72f80388c32ed44edf3801b4865f70b72
Jack Roudebush <roudebushjack@gmail.com>
2023-05-06 23:34:01 -0400

    Create a setter method to get user input for the guess

--- player.rb
+++ player.rb
@@ -1,7 +1,13 @@
 class Player
-  attr_accessor :guess
+  attr_reader :guess
 
   def initialize
     @guess = nil
   end
+
+  def guess=(*)
+    puts 'Enter your code guess: '
+    @guess = gets.chomp.split('')
+    p guess
+  end 
 end
