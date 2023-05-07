f1be08acfd1f2c02e97776a274415da2bcfb4374
Jack Roudebush <roudebushjack@gmail.com>
2023-05-07 16:25:55 -0400

    Create display_feedback method. Not finished

--- game.rb
+++ game.rb
@@ -13,6 +13,12 @@ class Game
     @rounds = 1
   end
 
+  def display_feedback(user_guess, computer_code)
+    user_guess.each { |num| print num + " " }
+    print "  Clues: "
+    puts " "
+  end
+
   def play
     while rounds <= 12
       p "Round ##{rounds}: "
@@ -23,6 +29,7 @@ class Game
       break if computer.code_correct?(human.guess)
 
       incorrect_guess(human.guess, rounds)
+      display_feedback(human.guess, computer.code)
       self.rounds += 1
     end
     computer.display_code
