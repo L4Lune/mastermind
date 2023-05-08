ad64d7ba3ba3007a50ab12496e1c83cd74a03e72
Jack Roudebush <roudebushjack@gmail.com>
2023-05-07 22:34:37 -0400

    Work out logic for feedback

--- game.rb
+++ game.rb
@@ -5,26 +5,42 @@ class Game
   include Icons
   include TextPrompts
 
-  attr_reader :human, :computer
-  attr_accessor :rounds
+  attr_reader :human, :computer 
+  attr_accessor :rounds, :exact_number, :same_number
 
   def initialize
     @human = Player.new
     @computer = CompPlayer.new
     @rounds = 1
+    @exact_number = 0
+    @same_number = 0
+    @user_guess_clone = []
+    @master = []
   end
 
-  def generate_feedback(user_guess, computer_code)
-    i =0
-    exact_matches = 0
-    same_matches = 0
-    for i in user_guess.length do
-      if user_guess[i] == computer_code[i]
-        exact += 1
-      elsif user_guess[i].include?(computer_code)
-        same += 1
-      end
+  def determine_exact_matches(user_guess, computer_code)
+    self.exact_number = 0
+    computer_code.each_with_index do |item, index|
+      next unless item == user_guess[index]
+
+      # computer_code[index] = '*'
+      # user_guess[index]  = '*'
+      self.exact_number += 1
     end
+    exact_number
+  end
+
+  def right_numbers(user_guess, computer_code)
+    self.same_number = 0
+    user_guess.each_index do |index|
+      next unless user_guess[index] != computer_code[index] && computer_code.include?(user_guess[index])
+
+      self.same_number += 1
+      remove = computer_code.find_index(user_guess[index])
+      # computer_code[remove] = '?'
+      # user_guess[index] = '?'
+    end
+    same_number
   end
 
   def play
@@ -38,8 +54,9 @@ class Game
 
       incorrect_guess(human.guess, rounds)
       display_guess(human.guess)
-      generate_feedback(human.guess, computer.code)
-      display_feedback(human.guess, computer.code)
+      determine_exact_matches(human.guess, computer.code)
+      right_numbers(human.guess, computer.code)
+      display_feedback(exact_number, same_number)
       self.rounds += 1
     end
     computer.display_code
